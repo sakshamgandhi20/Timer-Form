1a9722fd1fe12927b550600efae009e90a789583
sakshamgandhi20 <sakshamgandhi69@gmail.com>
2024-11-17 17:41:21 +0530

    backend url changed

--- src/service/axios-config.js
+++ src/service/axios-config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:2002";
+const baseURL = "https://timer-form-backend.onrender.com";
 
 const publicAxios = axios.create({ baseURL });
 
